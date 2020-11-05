b7cd053907f60d343a10f8348cae0800212aac58
chaitanya <patelchaitanya514@gmail.com>
2020-11-05 04:03:32 -0500

    Initial commit

---
+++ precache-manifest.7531a6cd2516b31c481412f87a52c23e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f62e7f999f1ccc6c83208dfb716c84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16357df7e7992d247c73",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "8dc8e40342b0bc408544",
+    "url": "/static/css/main.ebaa70d7.chunk.css"
+  },
+  {
+    "revision": "16357df7e7992d247c73",
+    "url": "/static/js/2.017bdf58.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.017bdf58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc8e40342b0bc408544",
+    "url": "/static/js/main.363a37be.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
